6bdee7770f280c66de91bf5588ede5f9c3104f46
BT_1N3_13 <bths@sample.edu>
2023-11-28 08:40:12 -0500

    v1.01

--- src/Blackjack.java
+++ src/Blackjack.java
@@ -1,23 +1,53 @@
 import java.util.Scanner;
 
-
+/**
+ * This class represents the user's object
+ *
+ * @author Tony Tran
+ */
 public class Blackjack {
+    /** The total value of the user's hand */
     private int hand;
-    private String previousCard;
+    /** The  value of the user's previous card */
+    private int previousCard;
+    /** The total value of money the user has */
     private int cash;
+    /** The value the user will bet */
     private int bet;
+
+    /**
+     * Instantiates a Blackjack object
+     * <p>
+     * No parameter but will set default values
+     */
 
     public Blackjack() {
         hand = 0;
-        previousCard = "";
+        previousCard = 0;
         cash = 1000;
         bet = 0;
     }
 
-    public void start() {
+    /**
+     * Starts the game and welcomes the user.
+     *
+     * @param name The name of the user
+     */
+    public void start(String name) {
         Robit player2 = new Robit();
-        System.out.println("Welcome to Blackjack.");
+        System.out.println("Welcome to Blackjack " + name + ".");
 
+    }
+
+    /**
+     * Draws a card at random and adds it to the value of hand
+     *
+     * @return The value of the previous card
+     */
+    public int drawCard() {
+        previousCard = (int) (Math.random() * 14) + 1;
+        hand += previousCard;
+        return previousCard;
     }
 
 }
